d967fab93e4f1104757a70c19548a978ae661333
AasthaSinha2305 <55781193+AasthaSinha2305@users.noreply.github.com>
2020-10-17 22:42:34 +0530

    footer and navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './App.css';
+import {Navbar} from "./Navbar"
+import './Navbar.css'
+import {Footer} from './Footer'
+import './Footer.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Navbar/>
+       
         <p>
          Welcome to MuckIn!!
         </p>
-        </header>
+      
+      </header>
+      <Footer/>
     </div>
   );
 }
