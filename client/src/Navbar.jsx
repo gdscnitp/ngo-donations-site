864517aa5ebd0275673bacdc4ffb860103c81ea8
arnav1507 <55322170+arnav1507@users.noreply.github.com>
2020-10-18 14:03:26 +0530

    Update Navbar.jsx

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <img src={c4} className="nav-img" alt="Logo" />
-  <a class="navbar-brand" href="#"> &nbsp;NGO</a>
+  <a class="navbar-brand" href="#top"> &nbsp;NGO</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -15,10 +15,10 @@ const Navbar = () => {
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto">
     <li className="nav-item">
-      <a id="search" href="#" class="nav-link">Search &nbsp;<i class="fa fa-search" aria-hidden="true"></i></a>
+      <a id="search" href="#top" class="nav-link">Search &nbsp;<i class="fa fa-search" aria-hidden="true"></i></a>
       </li>
       <li  className="nav-item">
-      <a id="sign-in" href="#" className="nav-link" role="button">&nbsp; Sign in &nbsp;<i class="fa fa-sign-in" aria-hidden="true"></i></a>
+      <a id="sign-in" href="#top" className="nav-link" role="button">&nbsp; Sign in &nbsp;<i class="fa fa-sign-in" aria-hidden="true"></i></a>
       </li>
     </ul>
   </div>
