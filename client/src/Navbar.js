76ee3c447183d15e3166a3877e1cd28fa1fb0ae1
AasthaSinha2305 <55781193+AasthaSinha2305@users.noreply.github.com>
2020-10-18 23:16:00 +0530

    changes made

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -20,7 +20,7 @@ export const Navbar = () => {
     
       <div class="menu menu--active" id="menu">
         <div class = "logo">
-        <img src="logo192.png" alt="Aastha Sinha Image" class="picture"></img>
+        <img src="logo192.png" alt="react Image" class="picture"></img>
         <div class = "search-bar">
           <SearchBar></SearchBar>
         </div>
@@ -32,19 +32,19 @@ export const Navbar = () => {
           <div class="menu__wrapper d-none d-lg-block col-md-12" >
             <nav class="">
               <ul>
-                <li><a href="#">Signup/Login</a></li>
-                <li><a href="#">FAQs</a></li>
-                <li><a href="#">About us</a></li>
-                <li><a href="#">Events</a></li>
+                <li><a href="https://www.google.co.in/" target="_blank">Signup/Login</a></li>
+                <li><a href="https://www.google.co.in/" target="_blank">FAQs</a></li>
+                <li><a href="https://www.google.co.in/" target="_blank">About us</a></li>
+                <li><a href="https://www.google.co.in/" target="_blank">Events</a></li>
               </ul>
             </nav>
           </div>
           <div id="mySidebar" class="sidebar">
             <a href="javascript:void(0)" class="closebtn" onClick={closeNav}>×</a>
-            <a href="#">Events</a>
-            <a href="#">About us</a>
-            <a href="#">FAQs</a>
-            <a href="#">Signup/Login</a>
+            <a href="https://www.google.co.in/" target="_blank">Events</a>
+            <a href="https://www.google.co.in/" target="_blank">About us</a>
+            <a href="https://www.google.co.in/" target="_blank">FAQs</a>
+            <a href="https://www.google.co.in/" target="_blank">Signup/Login</a>
           </div>
           <div class="menu__wrapper col-md-12 d-lg-none" >
             <button type="button" class="menu__mobile-button" onClick={openNav}>
--- client/src/SearchBar.js
+++ client/src/SearchBar.js
@@ -6,7 +6,7 @@ export const SearchBar = () => {
 
     <div class="search-container">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-        <form action="">
+        <form>
             <input type="text" placeholder="Search organisation/NGO" name="search" size = "25"></input>
             <button type="submit"><i class="fa fa-search"></i></button>
         </form>
