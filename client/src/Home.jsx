dc8037e8885801393e7a19bd2dc19f5df03a2ee9
arnav1507 <55322170+arnav1507@users.noreply.github.com>
2020-10-18 14:18:52 +0530

    Update Home.jsx

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         
                 <div className="row">
                 <div className="col-lg-12 order-1 order-lg-2">
-                    <img src={ cover } className="img" alt="Cover Image" />
+                    <img src={ cover } className="img" alt="Cover" />
                 </div>
                 </div>
                
