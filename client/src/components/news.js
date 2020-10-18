297b7a98739d9a6384b46e1693870b19dbe90bcd
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2020-10-18 07:50:46 +0530

    Started to add news component

--- client/src/components/news.js
+++ client/src/components/news.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Newslist from './subComponents/newslist';
 
 export default class News extends Component {
     render() {
         return (
             <div>
-                
+                <div className="news-container">
+                    <div className="news-display">
+   
+                    </div>
+                    <div className="news-list">
+                        <Newslist />
+                    </div>
+                </div>
             </div>
         )
     }
---
+++ client/src/components/subComponents/newslist.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+export default class Newslist extends Component {
+    state=[
+        {
+            title: "Kanpur city under poverty",
+            desc: "this tells that all the buisness settled over here is going down and down which is not a good sign for the upcoming days"
+        },
+        {
+            title: "Varnasi",
+            desc: "The city oh bhole"
+        }
+    ]
+    render() {
+        var list =[]
+        this.state.forEach(news => {
+            list.push(<p>
+                <div className="card">
+                <div className="header">
+                  <h2>{news.title}</h2>
+                </div>
+                <div className="desc">
+                    {news.desc}
+                </div>
+                </div></p>);
+            
+        });
+        return (
+            <div>
+               <div className="list-container">
+                    {list}
+                </div>
+            </div>
+        )
+    }
+}
