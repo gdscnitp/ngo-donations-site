c99f78f6ef1898716f7382de0dc28416f354b5b4
pallavi sharma <44518437+pallavisharma26@users.noreply.github.com>
2020-10-19 02:17:54 -0700

    Delete token.js

--- server/models/token.js
+++
@@ -1,5 +0,0 @@
-var tokenSchema = new mongoose.Schema({
-    _userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
-    token: { type: String, required: true },
-    expireAt: { type: Date, default: Date.now, index: { expires: 86400000 } }
-});
