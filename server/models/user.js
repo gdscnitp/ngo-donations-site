cb36186475fe63047441b515c8f209fe92c8e695
Vishal559 <vishalkumar.s00136@gmail.com>
2020-10-20 21:03:26 +0530

    Edit Profile Backend

---
+++ server/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//create schema for todo
+const UserSchema = new Schema({
+  _id : mongoose.Schema.Types.ObjectId,
+  UserName: {
+    type: String,
+    required: [true, 'The todo text field is required']
+  },
+  profileImage:{
+       type: String,
+  },
+})
+
+//create model for todo
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
