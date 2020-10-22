4c3501f115d6d857a5bfafb03a38e04828a27afa
Vishal559 <vishalkumar.s00136@gmail.com>
2020-10-22 14:47:37 +0530

    change in app.js

--- server/app.js
+++ server/app.js
@@ -1,55 +1,11 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
-// const routes = require('./routes/api');
-// const path = require('path');
-// require('dotenv').config();
-//
-// const app = express();
-// app.use(express.static(path.join(__dirname, 'public')));
-//
-// const port = process.env.PORT || 5000;
-//
-// //connect to the database
-//
-// mongoose.connect(process.env.DB,{ useNewUrlParser: true , useUnifiedTopology: true })
-//   .then(() => console.log(`Database connected successfully`))
-//   .catch(err => console.log(err));
-//
-// //since mongoose promise is depreciated, we overide it with node's promise
-// mongoose.Promise = global.Promise;
-//
-// const RateLimit = require('express-rate-limit');
-// const limiter = new RateLimit({
-//   windowMs: 1*60*1000, // 1 minute
-//   max: 5
-// });
-// app.use(limiter);
-//
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-//
-// app.use(bodyParser.json());
-//
-// app.use('/api', routes);
-//
-// app.use((err, req, res, next) => {
-//   console.log(err);
-//   next();
-// });
-//
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`)
-// });
+
 const app = express();
 const mongoose = require('mongoose');
 const { join } = require('path');    //for getting path of the static directory
 const { exit } = require('process');
 const logger = require('morgan');
-const userRouter = require('./routes/user.js');
+const userRouter = require('./routes/api.js');
 const morgan = require('morgan');
 require('dotenv').config();
 
