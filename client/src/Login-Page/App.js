6ceb79ed00cebb228a59375afd6790b4591e83af
Palak Kumari <71482509+PalakKumari@users.noreply.github.com>
2020-10-22 19:53:36 +0530

    Added login page

---
+++ client/src/Login-Page/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Body} from "./component/Body"
+
+export class App extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render()
+  {
+    return(
+       <Body />
+    )
+  }
+}
+
+export default App;
