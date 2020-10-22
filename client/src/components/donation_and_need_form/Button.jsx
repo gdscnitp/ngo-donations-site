f10740a95b1b9f216123076be3469147c2efc5d0
aarushiarora-2610 <aarushi.arora.2610@gmail.com>
2020-10-22 16:21:10 +0530

    The required changes are done

---
+++ client/src/components/donation_and_need_form/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Button = () => {
+    return(
+        <div className="button">
+            <button type="button" class="btn btn-success">Submit for Approval</button>
+        </div>
+    )
+}
+export default Button;
