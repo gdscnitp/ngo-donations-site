1a3c8e3a1998f59ec0f08cf63d86d05b6d081757
Rakshita Jain <51185686+raksh543@users.noreply.github.com>
2020-10-25 21:44:50 +0530

    adding route

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Switch,Route} from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar } from "../src/components/Navbar/Navbar";
@@ -6,52 +7,36 @@ import { Footer } from "../src/components/Footer/Footer";
 import Donation from "../src/components/donation_and_need_form/donation-form.js";
 import Need from "../src/components/donation_and_need_form/need-form.js";
 import MyBook from "./components/home/book";
-import Rating from "./components/home/Rating";
-import Cause from "./components/home/cause";
 import Stories from "./components/home/stories";
-import News from "./components/home/news";
-import {Loginpage} from "./components/Login-page/Login-page";
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
+
+    <Router>
     <div className="App">
-      <Navbar></Navbar>
-      <header className="App-header">
-        <p>Welcome to MuckIn!!</p>
-      </header>
-      <br />
-      <br />
-      <center>
-        <h3>
-          <strong>Recent Events</strong>
-        </h3>
-        <br />
-        <br />
-        <Stories />
-        <br />
-        <br />
-        <h3>
-          <strong>Why Should I Care?</strong>
-        </h3>
-        <News />
-        <Cause />
-        <h3>
-          <strong>About Us BOOK!</strong>
-        </h3>
-        <MyBook />
-        <br />
-        <br />
-        <h3>What Do People Say?</h3>
-        <br />
-        <Rating />
-      </center>
-
-      <Donation />
-      <Loginpage />
-      <Need />
-
-      <Footer />
+       <header className="App-header">
+         <p>Welcome to MuckIn!!</p>
+       </header>
+       <a href="/donate" style={{float: "left"}}>Donate</a>
+       <a href="/need" style={{float: "right"}}>Need</a>
+       <br />
+       <br />
+        <Navbar />
+        <center>
+        <Switch>
+        <Route path="/" exact component={Homepage} />
+        <Route path="/aboutus" component={MyBook}/>
+        <Route path="/faqs" component={Stories}/>
+        <Route path="/signup" component={Homepage}/>
+        <Route path="/donate" component={Donation}/>
+        <Route path="/need" component={Need}/>
+        
+        </Switch>
+        </center>
+        <Footer />
     </div>
+  </Router>
   );
 }
 
