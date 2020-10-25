56f83cc5836c1f0175426519fb2e0a14a2e15228
ayeshaqaisar <56070872+ayeshaqaisar@users.noreply.github.com>
2020-10-26 03:40:37 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Homepage from './pages/Homepage';
 
 function App() {
   return (
+       <Router>
     <div className="App">
        <header className="App-header">
          <p>Welcome to MuckIn!!</p>
