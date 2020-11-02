07155c5e69447cf5457f1616b1815056b739a87d
Aditya Gupta <adityag.ug19.cs@nitp.ac.in>
2020-11-02 15:27:32 +0530

    Limit Fix, and formatting

--- server/app.js
+++ server/app.js
@@ -1,175 +1,67 @@
-const express = require('express');
-const rateLimit = require('express-rate-limit')
-//const { body } = require('express-validator');
+const express = require("express");
+const app = express();
+const rateLimit = require("express-rate-limit");
+const mongoose = require("mongoose");
+const { join } = require("path");
+const { exit } = require("process");
+const userRouter = require("./routes/user");
+const activitiesRouter = require("./routes/activities");
+const signupRouter = require("./routes/sign_up");
 
-const mongoose = require('mongoose');
-const { join } = require('path'); 
-const bcrypt = require('bcryptjs')   //for getting path of the static directory
-const { exit } = require('process');
-const logger = require('morgan');
-const userRouter = require('./routes/user.js');
-
-const morgan = require('morgan');
-require('dotenv').config();
-const User=require('./models/person');
-//const Event=require('./models/event');
+const morgan = require("morgan");
+require("dotenv").config();
 const PORT = process.env.PORT || 3000;
-const DB_NAME = 'events'        // later change it according to database
+const DB_NAME = "muckin_testing"; // later change it according to database
 const MONGO_DB_URI = `mongodb+srv://dscnitp_webdept_muckin:dscnitp_webdept_muckin@cluster0.kokfw.gcp.mongodb.net/project2?retryWrites=true&w=majority`;
-mongoose.connect( MONGO_DB_URI , 
-  {
+mongoose
+  .connect(MONGO_DB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
     dbName: DB_NAME,
     //w: 'majority'
-}).catch(err => { console.error(`Error in DB connection: mongo DB couldn't be reached`); exit(1); });
+  })
+  .catch((err) => {
+    console.error(`Error in DB connection: mongo DB couldn't be reached`);
+    exit(1);
+  });
 
 const db = mongoose.connection; //access to the pending connection
-db.on('error', (err) => {
-        console.log(`Error in DB connection`)
+db.on("error", (err) => {
+  console.log(`Error in DB connection`);
 });
-db.once('open', () => {
-    console.log(`Connected to the database : ${DB_NAME}`);
-})
-const limiter  = rateLimit({
-  windowMs:24*60*60*1000,
-  max:100
+db.once("open", () => {
+  console.log(`Connected to the database : ${DB_NAME}`);
 });
-var string = require('string-sanitizer')
-var sanitizer=require('sanitize')();
 
-const app = express();
-app.use( morgan('dev') );   //to log requests made to api
-app.use( express.urlencoded({extended: false}) );
-app.use( express.json() );
-app.use( express.static( join( __dirname, 'public'  ) ) );
-
-
-// app.set('view engine','ejs')
-
-
-
-
-
-
-
-// app.get("/sign_up", (req, res)=>{
-//
-// res.render("sign_up")
-// });
-
-app.post("/sign_up",(req,res)=>{
-bcrypt.hash(req.body.password,10,(err,hash)=>{
-if(err){
-  console.log('Password can not be encrypted')
-}
-user= new User({
-
- name:req.body.name,
- contactNumber:req.body.contactNumber,
- email:req.body.email,
- password:hash
-})
-user.save().then(()=>{
- console.log(user)
-})
-
-
-
-
-})
-
-
-
-
-
-  // res.redirect('/sign_up/org')
-
+app.use(
+  rateLimit({
+    // preliminary rate limit
+    windowMs: 24 * 60 * 60 * 1000,
+    max: 100,
   })
+);
+app.use(morgan("dev")); //to log requests made to api
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(join(__dirname, "public")));
 
+// Routes START
+app.use("/user", userRouter); // login, logout
+app.use("/sign_up", signupRouter); // sign_up individual and organisation
+app.use("/activities", activitiesRouter); // image, update-details, delete-details
+// Routes END
 
-// app.get("/sign_up/org", (req, res)=>{
-//     res.render("sign_up")
-// });
-
-app.post("/sign_up/org",limiter,(req,res)=>{
-
-  user.Name_of_organisation = string.sanitize(req.body.Name_of_organisation)
-  user.Address_of_organisation = string.sanitize(req.body.Address_of_organisation)
-  user.License_number=string.sanitize(req.body.License_number)
-  user.Type_of_organisation=string.sanitize(req.body.Type_of_organisation)
-    user.Description_of_organisation=string.sanitize(req.body.Description_of_organisation)
-      user.Volunteers_number=string.sanitize(req.body.Volunteers_number)
-      user.Type_of_help=string.sanitize(req.body.Type_of_help)
-      user.Open_for_volunteers=string.sanitize(req.body.Open_for_volunteers)
-// console.log(typeof(req.body.Address_of_organisation))
-// console.log(typeof(req.body.Volunteers_number))
-
-        User.updateOne({_id:user._id},{
-          name:user.name,
-          email:user.email,
-          contactNumber:user.contactNumber,
-          password:user.password,
-          Name_of_organisation : user.Name_of_organisation,
-          Address_of_organisation : user.Address_of_organisation,
-            License_number:user.License_number,
-            Type_of_organisation:user.Type_of_organisation,
-          Description_of_organisation:user.Description_of_organisation,
-            Volunteers_number:user.Volunteers_number,
-            Type_of_help:user.Type_of_help,
-            Open_for_volunteers:user.Open_for_volunteers}).then(()=>{
-            console.log(req.body.Volunteers_number)
-            console.log(user.Name_of_organisation)
-            console.log(user.Address_of_organisation)
-            })
-
-// res.redirect("/sign_up/org/preview")
-
-})
-
-// app.get("/sign_up/org/preview",(req,res)=>{
-//   res.render("preview",{
-//     user:user
-//   })
-// })
-
-// app.post("/sign_up/org/preview",(req,res)=>{
-//   user.isChecked=true
-//   User.updateOne({_id:user._id},{
-//     user:user}).then(()=>{
-//     console.log(user)
-//   })
-//
-// })
-
-
-
-
-app.get('/', function(req, res){
-  console.log("Root Route")
-  res.json({ message: "hello world" });
+//404 and Error handlers
+app.use((req, res, next) => {
+  //catch any request to endpoint not available
+  next({ status: 404, message: `Route ${req.baseUrl} not found` }, req, res);
+});
+app.use((err, req, res, next) => {
+  //error handler
+  res
+    .status(err.status || 500)
+    .send(err.message || `Request couldn't be completed`);
 });
 
-
-
-app.use('/user', userRouter);
-app.use('/event', require('./routes/event'));
-
-
-
-
-
-    //404 and Error handlers
-app.use( (req, res, next) => {  //catch any request to endpoint not available
-    next({status: 404, message: `Route ${req.baseUrl} not found`}, req, res);
-})
-app.use( (err, req, res, next) => { //error handler
-    res.status( err.status || 500 ).send(err.message || `Request couldn't be completed`);
-})
-
-
-app.listen(
-    PORT,
-    console.log(`Server listening on ${PORT}`)
-);
+app.listen(PORT, console.log(`Server listening on ${PORT}`));
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,83 +1,87 @@
-const express = require ('express');
+const express = require("express");
 const router = express.Router();
-const multer = require('multer');
-const User = require('../models/user');
+const multer = require("multer");
+const User = require("../models/user");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/uploads/')
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now()+file.originalname)
-    }
-  });
+  destination: function (req, file, cb) {
+    cb(null, "./public/uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
-  const fileFilter=(req, file, cb)=>{
-   if(file.mimetype ==='image/jpeg' || file.mimetype ==='image/jpg' || file.mimetype ==='image/png'){
-       cb(null,true);
-   }else{
-       cb(null, false);
-   }
-
-}
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/png"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 var upload = multer({
-    storage:storage,
-    limits:{
-        fileSize: 1024 * 1024 * 5 // later updated;
-    },
-    fileFilter:fileFilter
- });
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5, // later updated;
+  },
+  fileFilter: fileFilter,
+});
 
 // image path
 // limit: 5mb
 // filter : png, jpeg,jpg
-                                              // later checkAuth added to check user signin or not;
-router.patch("/update-profile/:id",upload.single('profileImage'),function(req,res,next){
+// later checkAuth added to check user signin or not;
+router.patch("/update-profile/:id", upload.single("profileImage"), function (
+  req,
+  res,
+  next
+) {
+  const id = req.params.id;
+  const info = req.body.UserName;
+  const profilePic = req.file.path;
 
-    const id = req.params.id;
-    const info = req.body.UserName;
-    const profilePic = req.file.path;
-
-    userModel.findById(id,function(err,data){
-      data.profileImage = profilePic ? profilePic : data.profileImage;
-      data.UserName = info;
-        data.save()
-          .then(doc=>{
-             res.status(201).json({
-                 message:"Profile Image Updated Successfully",
-                 results:doc
-             });
-          })
-          .catch(err=>{
-              res.json(err);
-          })
-
-     });
-
- });
-
- // update records route
-router.patch("/update-info/",function(req,res,next){ // check added later
-
-          const id = req.params.id;
-          const Info = req.body.UserName;
-          userModel.findById(id,function(err,data){
-
-          data.UserName = Info ? Info : data.UserName;
-                data.save()
-                .then(doc=>{
-                   res.status(201).json({
-                       message:"Category Updated Successfully",
-                       results:doc
-                   });
-                })
-                .catch(err=>{
-                    res.json(err);
-                })
-
-           });
+  userModel.findById(id, function (err, data) {
+    data.profileImage = profilePic ? profilePic : data.profileImage;
+    data.UserName = info;
+    data
+      .save()
+      .then((doc) => {
+        res.status(201).json({
+          message: "Profile Image Updated Successfully",
+          results: doc,
+        });
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 });
 
+// update records route
+router.patch("/update-info/", function (req, res, next) {
+  // check added later
+
+  const id = req.params.id;
+  const Info = req.body.UserName;
+  userModel.findById(id, function (err, data) {
+    data.UserName = Info ? Info : data.UserName;
+    data
+      .save()
+      .then((doc) => {
+        res.status(201).json({
+          message: "Category Updated Successfully",
+          results: doc,
+        });
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+});
 
 module.exports = router;
