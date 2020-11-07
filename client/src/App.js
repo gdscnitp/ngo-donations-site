c539ab2687a3ce8c5123fe0fd37ff96fe10c505c
aarushiarora-2610 <aarushi.arora.2610@gmail.com>
2020-11-07 22:41:59 +0530

    Carousel added

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,8 @@ import Stories from "./components/home/stories";
 import User from  "./components/User-profile/User.js";
 import {Loginpage} from "./components/Login-page/Login-page";
 import Homepage from "./pages/Homepage";
-
-import Event from "./components/Event/Event.js"
+import Event from "./components/Event/Event.js";
+import Carousel from "./components/carousel/carousel.js";
 
 import Events from "./components/Event Page/Events";
 
@@ -23,6 +23,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
+          <Carousel />
           <p>Welcome to MuckIn!!</p>
         </header>
       <User/>
