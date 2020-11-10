d6a7b2cd339bcb1539e8ff9b9cf5352da79381a9
arnav1507 <arnavranjan15@gmail.com>
2020-11-11 01:17:58 +0530

    fixed tab issue

---
+++ client/src/components/Feed Page/Tabbs.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Tabbs = () => {
+  return (
+    <>
+      <div className="container col-12 mb-0">
+        <div class="tabContainer">
+          <div class="buttonContainer">
+            <button className="tab-btn-1">Activities</button>
+            <button className="tab-btn-2">Requests</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Tabbs;
