36025abea698610acf07977f218d3c885d4470d0
Aditya Gupta <adityag.ug19.cs@nitp.ac.in>
2020-12-19 19:47:02 +0530

    Update deploy.ign.js

--- server/deploy.ign.js
+++ server/deploy.ign.js
@@ -4,4 +4,4 @@ setTimeout(() => {
     lserver.close();
     console.log(`Exiting gracefully :D`);
     process.exit(0);    //OK
-}, 5000);   // waits for 5 seconds before exiting
+}, 6000);   // waits for 5 seconds before exiting
