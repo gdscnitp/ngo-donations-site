a984223c4c48e4a79e367c2c9bc66df61ec7ae1f
Rupali0311 <rupalitiwary0311@gmail.com>
2021-01-19 01:13:32 +0530

    Need form backend merged with frontend

---
+++ server/models/Request.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+console.log('Schema')
+
+const RequestSchema = new Schema({
+desc_problem:{
+    type:String
+    
+},
+desc_need:{
+    type:String
+    
+},
+location:{
+    type:String
+    
+},
+help_required_for:{
+    type:String
+    
+},
+no_of_persons:{
+    type:Number
+   
+}
+
+})
+const Requests = mongoose.model("Request",RequestSchema)
+module.exports = Requests
