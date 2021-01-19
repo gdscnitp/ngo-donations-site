05bb15c4339f7bc4294a077347286f462130d258
PalakKumari <palakkumari404@gmail.com>
2021-01-20 01:09:59 +0530

    Added dropdown menu

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import store from "./store";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
