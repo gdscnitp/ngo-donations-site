18912205453bc3a1a79eadcb80c0a465ff2835a0
Aditya Gupta <adityag.ug19.cs@nitp.ac.in>
2021-01-23 20:33:56 +0530

    Now deployments should be connected too

--- client/src/constants/baseurl.js
+++ client/src/constants/baseurl.js
@@ -1,6 +1,6 @@
 // @note - All these routes will be without a trailibg forward slash '/', so concat endpoints yourselves
 // @todo -> Replace `example.com` with the url to the deployed
-exports.BASE_API_ROUTE = process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://example.com";
+exports.BASE_API_ROUTE = process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://muckin-server.herokuapp.com/";
 
 exports.LOGIN_ROUTE = `${this.BASE_API_ROUTE}/user/login`;
 
