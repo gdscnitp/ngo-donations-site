f61f4146e7d6eb2e53806c6ccf5659e9e4203516
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2021-02-13 02:30:18 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -115,4 +115,13 @@ app.use((err, req, res, next) => {
     .send(err.message || `Request couldn't be completed`);
 });
 
+//Heroku deploy code
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, console.log(`Server listening on ${PORT}`));
