6711c3dc21dd01c694004c10006d3eaa855f7b74
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2021-02-13 03:08:54 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -101,7 +101,7 @@ app.use(
 );
 //Heroku deploy code
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("../client/build"));
   const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
