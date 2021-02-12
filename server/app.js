c35e9b0a8b2ec4111b738ec7e630d0686f86f91b
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2021-02-13 03:28:01 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -100,7 +100,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("../client/build"));
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
 
